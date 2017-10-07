2b81e0683e8933bea7a103f6ff03c06f744fa96f
piotr-aueternum <piotr.aueternum@gmail.com>
2017-10-07 16:26:40 +0200

    Cleanup (#1)
    * Code improvement, removed gulp; ADDED: eslint to rollup, few rules for better code uniformity, more information in README
    
    * Fixed typo in test and README

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
+import eslint from 'rollup-plugin-eslint';
+import eslintrc from './.eslintrc.json';
 
 export default {
-    entry: 'src/Form',
-    format: 'umd',
-    moduleName: 'JSLidate',
-    dest: 'dist/jslidate.js',
+    name: 'JSLidate',
+    input: 'src/Form',
+    output: {
+        file: 'dist/jslidate.js',
+        format: 'umd',
+    },
     plugins: [
-      globals(),
-      builtins(),
+        globals(),
+        builtins(),
+        eslint(eslintrc),
     ],
 };
