0c92ed1ef25eddae39185b212783d8759ce5631d
piotr-aueternum <piotr.aueternum@gmail.com>
2017-11-05 15:19:35 +0100

    Added error info functionality, few rules to eslintrc; Update docs and RuleTypes

--- src/RuleTypes.js
+++ src/RuleTypes.js
@@ -1,26 +1,29 @@
-/*
-* @param {Number} length - maximum length
-* @param {String} value - value to validate
-*
-* @returns {Boolean} - if value has passed the test function returns true
- */
-export const maxLength = length => value => value.length <= length;
+/**
+  * @param {Number} length
+  * @returns {(value: string) => ({validate: Boolean, message: Array})} - if value has passed the test function returns true
+  */
+export const maxLength = (length, pattern = length) => value => ({
+    validate: value.length <= length,
+    message: ['Maximal length', pattern],
+});
 
-/*
- * @param {Number} length - maximum length
- * @param {String} value - value to validate
- *
- * @returns {Boolean} - if value has passed the test function returns true
- */
-export const minLength = length => value => value.length >= length;
+/**
+  * @param {Number} length
+  * @returns {(value: string) => ({validate: Boolean, message: Array})} - if value has passed the test function returns true
+  */
+export const minLength = (length, pattern = length) => value => ({
+    validate: value.length >= length,
+    message: ['Minimal length', pattern],
+});
 
-/*
- * @param {Object} regexObj - regExp Object
- * @param {String} value - value to validate
- *
- * @returns {Boolean} - if value has passed the test function returns true
- */
-export const patternRule = regexObj => value => regexObj.test(value);
+/**
+  * @param {RegExp} regexObj
+  * @returns {(value: string) => ({validate: Boolean, message: Array})} - if value has passed the test function returns true
+  */
+export const patternRule = (regexObj, pattern = regexObj) => value => ({
+    validate: regexObj.test(value),
+    message: ['Field should match pattern', pattern],
+});
 
 const RuleTypes = { maxLength, minLength, patternRule };
 
